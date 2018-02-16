ed5e9dade0df1a22ab88e69c68b1a1adbd2852b8
Sky CH. Wang <sky.wang@yahoo.com>
2018-02-16 04:38:04 -0500

    reorganizing

--- randomOrderGenerator.py
+++ randomOrderGenerator.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def randomOrderGenerator(numGenerate=1000, orderLength=50, orderStreamSize=100, high=101, low=0):
+def randomOrderGenerator(numGenerate=1000, orderStreamSize=100, orderLength=50, high=101, low=0):
 	while True:
 		yield np.random.randint(low, high, size=(numGenerate, orderStreamSize, orderLength, 1))
 
@@ -9,8 +9,8 @@ def randomLabelGenerator(numGenerate=1000):
     yield np.random.randint(0, 2, size=(numGenerate))
 
 #Testing
-#r = randomOrderGenerator(2, 5, 3)
-#print(next(r))
+#r = randomOrderGenerator(1, 2, 5)
+#print(next(r)[0])
 #l = randomLabelGenerator()
 #print(next(l))
 
