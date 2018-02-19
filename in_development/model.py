58bd4c6a4c64de56972e320052d044d2d37efc7a
Sky CH. Wang <sky.wang@yahoo.com>
2018-02-18 20:57:45 -0500

    normalize

--- in_development/model.py
+++ in_development/model.py
@@ -4,7 +4,7 @@ from keras import backend as K
 from keras.models import Sequential
 from keras import regularizers
 from keras.layers import (Conv2D, MaxPooling2D, Dropout, Input, Activation, Flatten, Dense, Reshape, UpSampling2D, BatchNormalization)
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam
 from randomOrderGenerator import (randomOrderGenerator, randomLabelGenerator)
 
 import pdb
@@ -47,7 +47,7 @@ class GAN(object):
 		if self.DM:
 			return self.DM
 
-		optimizer = RMSprop()
+		optimizer = Adam()
 		self.DM = Sequential()
 		self.DM.add(self.discriminator())
 		self.DM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
@@ -83,7 +83,7 @@ class GAN(object):
 			return self.AM
 
 		# Stacked, generator model onto the discriminator
-		optimizer=RMSprop(lr=0.5)
+		optimizer=Adam()
 		self.AM = Sequential()
 		self.AM.add(self.generator())
 		self.AM.add(self.discriminator())
@@ -93,12 +93,9 @@ class GAN(object):
 
 class financial_GAN(object):
 	def __init__(self):
+		# generated orderstream dimensions
 		self.orderStreamSize = 100
 		self.orderLength = 50
-
-		# Random orderstream
-		self.numGenerate = 10000
-		self.x_train = next(randomOrderGenerator(self.numGenerate, self.orderStreamSize, self.orderLength))
 
 		# init
 		self.GAN = GAN(orderStreamSize=self.orderStreamSize, orderLength=self.orderLength)
@@ -106,25 +103,74 @@ class financial_GAN(object):
 		self.generator = self.GAN.generator()
 		self.adversarial = self.GAN.adversarial_model()
 
-	def train(self, train_steps=1000, batch_size=256):
+	def normalize(self, array, maxV=100, minV=0, high=1, low=-1):
+		return (high - (((high - low) * (maxV - array)) / (maxV - minV)))
+
+	def denormalize(self, normArray, maxV=100, minV=0, high=1, low=-1):
+		return ((((normArray - high) * (maxV - minV))/(high - low)) + maxV)
+
+
+	def train(self, train_steps=1000, batch_size=1024, pretrain_size=10000):
+		# Pretrain 10 epochs
+		x = self.normalize(next(randomOrderGenerator(pretrain_size, self.orderStreamSize, self.orderLength)))
+		y = np.ones([pretrain_size, 1])
+		print("\nPretrain of discriminator:\n")
+		self.discriminator.fit(x, y, epochs=10)
+
+		# USES TRAIN_ON_BATCH
+		# Real and Generated train
+		"""
 		for i in range(train_steps):
+			## gen noise init
 			noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-			orderStreams_train = self.x_train[np.random.randint(0, self.x_train.shape[0], size=batch_size), :, :, :]
+			## train/fake init
+			orderStreams_train = self.normalize(next(randomOrderGenerator(batch_size, self.orderStreamSize, self.orderLength)))
 			orderStreams_fake = self.generator.predict(noise)
+			## data/labels init
 			x = np.concatenate((orderStreams_train, orderStreams_fake))
-			y = np.ones([2*batch_size, 1])
+			y = np.ones([batch_size*2, 1])
 			y[batch_size:, :] = 0
+			## D training
 			d_loss = self.discriminator.train_on_batch(x, y)
-			y = np.ones([batch_size, 1])
+			## A=G+D training
 			self.discriminator.trainable = False
+			y = np.ones([batch_size, 1])
 			noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
 			a_loss = self.adversarial.train_on_batch(noise, y)
 			self.discriminator.trainable = True
-
+			# output
 			log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
 			log_mesg = "%s  [A loss: %f]" % (log_mesg, a_loss)
 			print(log_mesg)
-			#print(x[257])
+
+			if i % 10 == 0:
+				self.discriminator.save_weights('discriminator', True)
+				self.generator.save_weights('generator', True)
+		"""
+
+		# USES FIT
+		# Real and Generated train
+		for i in range(train_steps):
+			print("\nRound: " + str(i))
+			## gen noise init
+			noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+			## train/fake init
+			orderStreams_train = self.normalize(next(randomOrderGenerator(batch_size, self.orderStreamSize, self.orderLength)))
+			orderStreams_fake = self.generator.predict(noise)
+			## data/labels init
+			x = np.concatenate((orderStreams_train, orderStreams_fake))
+			y = np.ones([batch_size*2, 1])
+			y[batch_size:, :] = 0
+			## D training
+			print("Discriminator train: ")
+			d_loss = self.discriminator.fit(x, y, epochs=1, shuffle=True)
+			## A=G+D training
+			self.discriminator.trainable = False
+			noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+			y = np.ones([batch_size, 1])
+			print("Adversarial train: ")
+			a_loss = self.adversarial.fit(noise, y, epochs=1)
+			self.discriminator.trainable = True
 
 			if i % 10 == 0:
 				self.discriminator.save_weights('discriminator', True)
