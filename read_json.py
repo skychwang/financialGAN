ef5cddd7672dac6b53eddc7c3f13c14f3f6acac2
Sky CH. Wang <sky.wang@yahoo.com>
2018-03-09 16:26:48 -0500

    format

--- read_json.py
+++ read_json.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import os
+import sys
 
-def read_data(file_name,start_id,stream_size,batch_size):
+def read_one_day_data(file_name,start_id=0,stream_size=10,batch_size=500):
     '''
     Input:
     file_name : filename of json_file
@@ -38,4 +39,17 @@ def read_data(file_name,start_id,stream_size,batch_size):
                 buy_array[int(price)-1,time_index[i]-start] = size
             for price,size in sell_vector[i].items():
                 sell_array[int(price)-1,time_index[i]-start] = size
-    return buy_array.reshape(-1,stream_size,batch_size,order='F'),sell_array.reshape(-1,stream_size,batch_size,order='F')
+    buy_array = buy_array.reshape(batch_size,stream_size,-1,1,order='F')
+    sell_array = sell_array.reshape(batch_size,stream_size,-1,1,order='F')
+    return np.concatenate((buy_array, sell_array), axis=3)
+
+def read_multiple_days_data():
+    raw_orders = [file for file in os.listdir("output/") if file.endswith(".json")]
+    data = read_one_day_data(raw_orders[0])
+    for raw_order in raw_orders[1:]:
+        data = np.concatenate((data, read_one_day_data(raw_order)), axis=0)
+    print(data.shape)
+    return data;
+        
+if __name__ == '__main__':
+    np.save("data.npy", read_multiple_days_data())
