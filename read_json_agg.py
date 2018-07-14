fea2561793738f5ed490c7978f5275b02e9bc9cb
LIJUNYI95 <junyili@umich.edu>
2018-07-14 16:29:50 +0000

    update

--- read_json_agg.py
+++ read_json_agg.py
@@ -98,11 +98,11 @@ def read_multiple_days_data():
         raw_path = os.path.join('output/'+raw_orders[i])
         cancel_path = os.path.join('output_cancel/'+raw_orders_cancel[i])
 
-        tgt_path = os.path.join('NPY/'+raw_orders[i].replace('.json','.npy'))
+        tgt_path = os.path.join('NPY_1/'+raw_orders[i].replace('.json','.npy'))
         np.save(tgt_path, read_one_day_data(raw_path,cancel_path))
         #np.save(tgt_path, reshape_data(read_one_day_data(raw_path,cancel_path)))
 
-def reshape_data(buy_sell_array, zero_one=True, history=100,order_stream=1,step_size=1,batch_size=32):
+def reshape_data(buy_sell_array, zero_one=True, history=100,order_stream=10,step_size=10,batch_size=32):
     num_samples = int(np.floor((buy_sell_array.shape[0]-history - order_stream + step_size)/(step_size)));
     if zero_one:
         buy_sell_trun = np.zeros((num_samples, order_stream + history,4,1))
@@ -135,17 +135,17 @@ def reshape_data(buy_sell_array, zero_one=True, history=100,order_stream=1,step_
 
 
 
-def aggregate_multi_days_data(zero_one=True, history = 100, order_stream=1,step_size=1, batch_size=32):
+def aggregate_multi_days_data(zero_one=True, history = 100, order_stream=10,step_size=10, batch_size=32):
     raw_path = [file for file in os.listdir("NPY/") if file.endswith("_100.npy")]
     raw_data = np.load("NPY/" + raw_path[0])
     for i in range(1,len(raw_path)):
         raw_data = np.concatenate((raw_data,np.load("NPY/" + raw_path[i])))
 
     #Generate NPY(reshaped)
-    np.save("NPY/agg_data.npy",reshape_data(raw_data))
+    #np.save("NPY/agg_data.npy",reshape_data(raw_data))
 
     #Generate NPY_1(non-reshaped)
-    #np.save('NPY_1/agg_data.npy',raw_data)
+    np.save('NPY_1/agg_data.npy',raw_data)
 
 
 if __name__ == '__main__':
